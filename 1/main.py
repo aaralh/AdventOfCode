3260107635dca01d9051626a8cb6ebf823c7b718
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-12-06 09:13:04 +0200

    First day, second part

--- 1/main.py
+++ 1/main.py
@@ -1,13 +1,51 @@
+from typing import List
 
-currentFrequency = 0
 
 def readFile(fileName: str) -> str:
+    '''
+    Return contents of file with given name.
+
+    Arguments:
+    fileName -- Name of the file.
+    '''
     with open(fileName, 'r', newline='') as inputFile:
         return inputFile.read()
 
 
-frequencyValues = readFile("input.txt").splitlines()
-for value in frequencyValues:
-    currentFrequency = currentFrequency + int(value)
+def calculateCurrentFrequency(frequencyList: List[int], currentFrequency: int) -> int:
+    '''
+    Return frequency after applying all changes in frequencyList.
 
-print(currentFrequency)
+    Arguments:
+    frequencyList -- Frequency list which will be looped through.
+    '''
+    for value in frequencyList:
+        currentFrequency = currentFrequency + int(value)
+
+    return currentFrequency
+
+
+def calculateFrequencySeenTwice(frequencyList: List[int]) -> int:
+    '''
+    Loops through frequencyList first twice seen frequency is found and returns it.
+
+    Arguments:
+    frequencyList -- Frequency list which will be looped through.
+    '''
+    currentFreq = 0
+    seenFrequencys = {currentFreq} # Using set to gain some speed benefit.
+    frequencySeenTwice = None
+    while frequencySeenTwice == None:
+        for frequency in frequencyList:
+            currentFreq = currentFreq + int(frequency)
+            if currentFreq in seenFrequencys:
+                frequencySeenTwice = currentFreq
+                break
+            else:
+                seenFrequencys.add(currentFreq)
+
+    return frequencySeenTwice
+
+
+lines = readFile("input.txt").splitlines()
+frequencyList = [int(line) for line in lines]
